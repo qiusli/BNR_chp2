bc1c8a1c00fa69fd102f92be7e5fecd18f897dff
Qiushi Li <qius.li3@gmail.com>
2015-05-25 17:02:25 -0500

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Qiushi Li on 5/25/15.
+//  Copyright (c) 2015 BigNerdRanch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
